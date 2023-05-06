3a9f4266d7c1cb84b27639f6024a8d0feef38bc8
Rahul Chhabra <chhabrarahul2707@gmail.com>
2023-05-06 15:00:58 +0530

    Create and delete city methods added.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,28 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { PORT } = require('./config/SeverConfig');
+const CityRepository = require('./repository/city-repository');
 
-const setupAndStartServer = async() => {
+
+
+const setupAndStartServer = async () => {
+
     const app = express();
+
     app.use(bodyParser.json());
-    app.use(bodyParser.urlencoded({ extended: true}));
-    app.listen(PORT,() => {
+    app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.listen(PORT, async () => {
         console.log(`Server is started at port ${PORT}`);
+
+        const repo = new CityRepository();
+        try {
+            await repo.createCity({ name:"Los Angles"})
+            // await repo.deleteCity(1);
+        }
+        catch(error) {
+            console.log(error)
+        }
     })
 }
 setupAndStartServer(); 
